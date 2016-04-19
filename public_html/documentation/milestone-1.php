89ec186d39a6eba7337bd5c02a97a565e56bb34a
Kai Garrott <garrottkai@gmail.com>
2016-04-19 05:55:35 -0600

    Added headings

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -3,12 +3,18 @@
 	<head>
 		<meta charset="UTF-8" />
 		<meta name="author" content="Kai Garrott" />
-		<meta name="description" content="Milestone 1 project overview" />
+		<meta name="description" content="Milestone 1: Project Overview" />
 		<title>Overview</title>
 	</head>
 	<body>
 		<header>
-			<
+			<h1>Project Overview</h1>
+			<p>Personal Website Project</p>
 		</header>
+		<h2>Purpose</h2>
+		<h2>Audience</h2>
+		<h2>Goal</h2>
+		<h2>Persona</h2>
+		<h2>Use Case</h2>
 	</body>
 </html>
