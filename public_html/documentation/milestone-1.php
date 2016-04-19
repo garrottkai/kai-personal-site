b7287f8a9803a6777ff6382d2d17e58bea0cfd4c
Kai Garrott <garrottkai@gmail.com>
2016-04-19 06:57:28 -0600

    Added persona and use case

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -18,6 +18,21 @@
 		<h2>Goal</h2>
 			<p>This site is built with the intent of impressing upon the viewer the quality of my work, in order to convince him or her to employ my services in furtherance of their business goals.</p>
 		<h2>Persona</h2>
+			<h4>Name</h4>
+				<p>Allison Schroeder</p>
+			<h4>Age</h4>
+				<p>42</p>
+			<h4>Occupation</h4>
+				<p>Vice-President of Marketing for Coleman Chevrolet, an established and respected new and used auto dealership in the area. Allison is looking to hire a web developer to produce a new, mobile-friendly website to replace Coleman's current site, which has been updated in bits and pieces since its last major revamp in 2008.</p>
+			<h4>Technology Experience</h4>
+				<p>Allison is an expert, daily user of all components of the Microsoft Office suite, including PowerPoint, Excel, Word, and Outlook. In the office, she employs an 18-month-old HP all-in-one desktop with a 27-inch screen. Allison also uses a late-model 17" Dell Latitude notebook and a Samsung Galaxy S6 smartphone. Her preferred browser on all platforms is Firefox.</p>
+			<h4>Attitudes and Work Behaviors</h4>
+				<p>Allison has a full schedule, juggling many responsibilities as the head of marketing for a multimillion-dollar regional business. She is vetting several dozen developers, and needs to be able to quickly verify that a potential hire for this job possesses the requisite skills to create an impressive, attractive and smoothly functional website experience for her customers.</p>
+			<h4>Frustrations</h4>
+				<p>Allison is looking for an individual to conduct a major, top-to-bottom overhaul of her company's online presence. This requires the developer's complete commitment to the project for an extended period of up to two months, as well as substantial support thereafter. She has investigated two somewhat larger development companies in the area, and is not convinced that they will be able to provide the kind of personal attention that she and Coleman require in this undertaking. Allison also needs to complete the hiring process and begin the project before the end of the month.</p>
+			<h4>Goals</h4>
+				<p>Allison would like to narrow down her list of candidates to a shortlist of three by the end of the day, in order to present these options to the president of the dealership at 9:30 tomorrow morning. She has a window of two hours this afternoon in which to scan the websites of 30 different developers. Allison will therefore be spending, at most, four minutes viewing each website and portfolio.</p>
 		<h2>Use Case</h2>
+			<p>Allison has come up with a list of potential contractors based on a Google search for web developers in the area. It is 3 P.M. on a Thursday, and she needs to create a shortlist of candidates by 5:00 in order to leave work on time to pick up her three-year-old from daycare. Allison has had an exhausting, stressful week of organizing two different tradeshow presentations, negotiating advertising contracts, and now the pressing problem of the company's antiquated website. She has little energy and even less patience for any difficulty in viewing and navigating candidates' sites, and rather enjoys Ctrl-W'ing any that cause her frustration at this point. In order to catch her attention, a website needs to be visually striking and pleasing to use.</p>
 	</body>
 </html>
