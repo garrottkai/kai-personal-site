d5c2ee4dc4fea1771318ea5d3f892626baead33a
Kai Garrott <garrottkai@gmail.com>
2016-04-19 05:49:54 -0600

    Added milestone 1 overview page

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="author" content="Kai Garrott" />
+		<meta name="description" content="Milestone 1 project overview" />
+		<title>Overview</title>
+	</head>
+	<body>
+		<header>
+			<
+		</header>
+	</body>
+</html>
