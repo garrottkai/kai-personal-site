e77802456ddb1ce82d4a4052e00a0292b6cc2f80
Kai Garrott <garrottkai@gmail.com>
2016-04-19 06:05:21 -0600

    Populated headings

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -9,11 +9,14 @@
 	<body>
 		<header>
 			<h1>Project Overview</h1>
-			<p>Personal Website Project</p>
+				<p>Personal Website Project</p>
 		</header>
 		<h2>Purpose</h2>
+			<p>This site will serve as a digital business card, housing my personal portfolio of development projects, and presenting a clean, professional interface to attract potential clients.</p>
 		<h2>Audience</h2>
+			<p>The intended viewers of the site will be small business owners and marketing managers looking to create a fresh, modern digital presence.</p>
 		<h2>Goal</h2>
+			<p>This site is built with the intent of impressing upon the viewer the quality of my work, in order to convince him or her to employ my services in furtherance of their business goals.</p>
 		<h2>Persona</h2>
 		<h2>Use Case</h2>
 	</body>
