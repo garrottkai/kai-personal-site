a30657b381f6f8fd9141933ccd59823bf7510610
Kai Garrott <garrottkai@gmail.com>
2016-05-02 08:40:35 -0600

    Final additions; upload desktop wireframe

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -20,12 +20,16 @@
 				<li>About Me</li>
 			</ul><br>
 			<h2>Content Strategy</h2>
+			<p>The home page will contain a brief introduction to the services I offer. More detailed information can be found on the "About Me" page, and a clickable gallery of images will be located in the Portfolio. To minimize the page structure of the site, contact information will be included at the bottom of all pages, and will be visible at all times, as no scrolling will be required.</p>
 			<h2>Interaction Flow</h2>
+			<p>The user (Allison) will navigate to the home page and be presented with a very simple, refined layout. Upon reading the introduction, she will click on the "Profile" link and click on the first item. A large image of the selected site will load. She will examine it, then exit the enlarged view, click the arrow to the side and look at the next entry. After determining that my work is satisfactory, she will navigate to the last page, "About Me," and read the paragraph found there. Then, she will open my social media profiles from the conveniently located buttons, and save my email address.</p>
 			<h2>Wireframes</h2>
 			<p>As this site will only have three pages, direct links will be included to each without requiring a menu.</p>
 			<p>Content will ideally fit on a single screen. Vertical scrolling will not be required. Portfolio items will be selectable from left to right.</p><br>
-			<img id="wireframe" src="media/wireframe.png" alt="site wireframe" />
-
+			<h4>Mobile</h4>
+			<img id="wireframe" src="media/wireframe.png" alt="site wireframe" /><br>
+			<h4>Desktop</h4>
+			<img id="desktop-wireframe" src="media/desktop-wireframe.png" alt="desktop wireframe" />
 		</div>
 	</body>
 </html>
