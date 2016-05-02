525e2bfcc39a06ce1931481dcb992fc7665a8e3a
Kai Garrott <garrottkai@gmail.com>
2016-05-02 06:59:43 -0600

    Milestone 2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="author" content="Kai Garrott" />
+		<meta name="description" content="Milestone 2" />
+		<title>Milestone 2</title>
+		<link rel="stylesheet" type="text/css" href="style.css" />
+	</head>
+	<body>
+		<header>
+			<h1>Milestone 2</h1>
+		</header>
+		<div class="container">
+			
+		</div>
+	</body>
+</html>
