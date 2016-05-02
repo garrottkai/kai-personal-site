619057a831f9ed00fcdd3c5bda0b6c4175fd1c0c
Kai Garrott <garrottkai@gmail.com>
2016-05-02 07:24:57 -0600

    Update milestone 2

--- public_html/documentation/milestone-2.php
+++ public_html/documentation/milestone-2.php
@@ -12,7 +12,20 @@
 			<h1>Milestone 2</h1>
 		</header>
 		<div class="container">
-			
+			<h2>Site Map</h2>
+			<p>This website will consist of three pages:</p>
+			<ul>
+				<li>Home</li>
+				<li>Portfolio</li>
+				<li>About Me</li>
+			</ul><br>
+			<h2>Content Strategy</h2>
+			<h2>Interaction Flow</h2>
+			<h2>Wireframes</h2>
+			<p>As this site will only have three pages, direct links will be included to each without requiring a menu.</p>
+			<p>Content will ideally fit on a single screen. Vertical scrolling will not be required. Portfolio items will be selectable from left to right.</p><br>
+			<img src="media/wireframe.png" alt="site wireframe" />
+
 		</div>
 	</body>
 </html>
