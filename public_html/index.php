abed746d9680815b6b0071cd2be176b9aa10cf3c
Kai Garrott <garrottkai@gmail.com>
2016-05-22 18:03:06 -0600

    begin constructing site

---
+++ public_html/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta name="author" content="Kai Garrott" />
+		<link rel="stylesheet" href="css/styles.css" type="text/css" />
+		<title>Kai Garrott, Developer</title>
+	</head>
+	<body class="sfooter">
+		<div class="sfooter-content">
+			<div>
+
+			</div>
+		</div> <!-- sfooter-content -->
+		<footer>
+			<p class="footer-l">Kai Garrott</p>
+			<p class="footer-c">Contact info here</p>
+			<p class="footer-r">Social media buttons</p>
+		</footer>
+	</body>
+</html>
