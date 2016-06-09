edac7c2e195f55b9838c8678a369371f60949640
Kai Garrott <garrottkai@gmail.com>
2016-06-09 15:02:01 -0600

    use dependencies.php

--- public_html/about.php
+++ public_html/about.php
@@ -7,41 +7,8 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<title>About | Kai Garrott</title>
 
-		<!-- bootstrap compiled/minified css -->
-
-		<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
-
-		<!-- FontAwesome minified -------------->
-
-		<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
-
-		<!-- Google fonts ---------------------->
-
-		<link href='https://fonts.googleapis.com/css?family=Fjord+One' rel='stylesheet' type='text/css'>
-		<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
-
-		<!-- local css ------------------------>
-
-		<link rel="stylesheet" href="css/styles.css" type="text/css" />
-
-		<!-- HTML5 shiv and respond.js for legacy IE support -->
-
-		<!--[if lt IE 9]>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
-		<script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
-		<![endif]-->
-
-		<!-- jQuery for components -------------->
-
-		<script
-			src="https://code.jquery.com/jquery-2.0.0.min.js"
-			integrity="sha256-1IKHGl6UjLSIT6CXLqmKgavKBXtr0/jJlaGMEkh+dhw="
-			crossorigin="anonymous"></script>
-
-		<!-- bootstrap js ----------------------->
-
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
-
+		<?php require_once("php/dependencies.php");?>
+		
 	</head>
 	<body class="sfooter">
 		<div class="sfooter-content">
