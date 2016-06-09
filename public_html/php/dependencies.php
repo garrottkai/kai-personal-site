85648b3f419275b8fbc23e7ef90f34814518a76d
Kai Garrott <garrottkai@gmail.com>
2016-06-09 14:59:44 -0600

    add dependencies.php

---
+++ public_html/php/dependencies.php
@@ -0,0 +1,36 @@
+<!-- bootstrap compiled/minified css -->
+
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous" />
+
+<!-- FontAwesome minified -------------->
+
+<link type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" />
+
+<!-- Google fonts ---------------------->
+
+<link href='https://fonts.googleapis.com/css?family=Fjord+One' rel='stylesheet' type='text/css'>
+<link href='https://fonts.googleapis.com/css?family=Oxygen' rel='stylesheet' type='text/css'>
+
+<!-- local css ------------------------>
+
+<link rel="stylesheet" href="css/styles.css" type="text/css" />
+
+<!-- HTML5 shiv and respond.js for legacy IE support -->
+
+<!--[if lt IE 9]>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+<![endif]-->
+
+<!-- jQuery for components -------------->
+
+<script
+	src="https://code.jquery.com/jquery-2.0.0.min.js"
+	integrity="sha256-1IKHGl6UjLSIT6CXLqmKgavKBXtr0/jJlaGMEkh+dhw="
+	crossorigin="anonymous"></script>
+
+<!-- bootstrap js ----------------------->
+
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+
+
